03488a45462c570e294a778bb503db66f77050fc
Vineet <telangvineet@gmail.com>
2025-06-25 13:44:18 +0000

    optim 4 comment

--- LLMs/config-train.py
+++ LLMs/config-train.py
@@ -45,7 +45,7 @@ class config:
     # hyperparameters
     batch_size = 4 # how many independent sequences will we process in parallel?
     block_size = 1024 # what is the maximum context length for predictions?
-    vocab_size = 50304
+    vocab_size = 50304 # OPTIM 4 (along with grad clipping) brought dt from 95 to 90
 
     max_iters = 500
     eval_interval = 50
