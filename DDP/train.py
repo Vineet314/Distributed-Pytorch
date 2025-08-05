ae0eaa3393da4f0c820c2a01df7f2e7b3cd926e9
Vineet <telangvineet@gmail.com>
2025-08-05 11:25:18 +0000

    add training

--- DDP/train.py
+++ DDP/train.py
@@ -1,1 +1,303 @@
-# TBA
+import warnings ; warnings.filterwarnings("ignore")
+import os
+import math
+import torch
+import argparse
+import numpy as np
+
+from typing import Literal
+from time import perf_counter
+from model import LLM, LLMconfig
+from dataclasses import dataclass
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
+assert torch.cuda.is_available()
+
+# ______________DEVICE, DTYPE, DDP SETUP_________________
+
+init_process_group(backend='nccl')
+ddp_rank = int(os.environ['RANK'])
+ddp_local_rank = int(os.environ['LOCAL_RANK'])
+ddp_world_size = int(os.environ['WORLD_SIZE'])
+device = f"cuda:{ddp_local_rank}"
+master_process = ddp_rank == 0
+if master_process : print(f"DDP_WORLD_SIZE = {ddp_world_size}")
+
+torch.cuda.set_device(device)
+torch.manual_seed(1729 + ddp_rank)         # offset the seed
+torch.cuda.manual_seed(1729 + ddp_rank)    # offset the seed
+torch.set_float32_matmul_precision('high') # Not sure if this has any effect when used with Auto Mixed Precision
+torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+
+dtype = 'float16' # if not torch.cuda.is_bf16_supported else 'bfloat16'
+ctx = torch.amp.autocast(device_type="cuda", dtype=getattr(torch, dtype))
+scaler = torch.amp.GradScaler(enabled=(dtype == 'float16'))
+
+# ____________PARAMS-CONFIG_________________
+
+@dataclass
+class Trainconfig:
+    dataset : str | Literal['shakespeare', 'tinystories']
+    total_batch_size : int
+    batch_size : int
+    max_iters : int
+    eval : bool
+    eval_interval : int
+    eval_iters : int
+    learning_rate : float
+    warmup_steps : int
+    grad_clip : int
+    save_model : bool
+
+ModelConfig = LLMconfig(
+    # token params
+    vocab_size = 50304, 
+    block_size = 2**10,
+    n_embd = 768, 
+    pos_emb = 'rope',
+    
+    # MoE
+    moe = True,
+
+    up_dim = 512, 
+    non_linearity = 'gelu',  
+    dropout=0.15,
+    n_layer = 9,
+
+    n_exp = 32,
+    n_shared = 1,
+    n_act = 8,        ### INCLUDES THE SHARED EXPERTS
+
+    coeff=0.01,
+    aux_free=True,
+    alpha = 0.0001,
+    gamma = 0.001,
+
+    # Attention
+    attn = 'mla', 
+    n_head = 8,
+    n_kv_heads=4,
+    # MHLA
+    q_latent_dim = 256, 
+    kv_latent_dim = 256,
+    rope_head_dim = 256)              
+
+TrainingConfig = Trainconfig(
+    dataset='tinystories',
+    total_batch_size = 2**12,
+    batch_size = 2**2, # how many independent sequences will we process in parallel?
+    max_iters = 2500,
+    eval = True,
+    eval_interval=100,
+    eval_iters=100,
+    learning_rate = 3e-4,
+    warmup_steps = 100,
+    grad_clip = 1.0,    
+    save_model = False)
+
+# ___________ CLI-OVERRIDE__________________
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a simple LLM model')
+    # Training Parameters
+    parser.add_argument('--dataset',       type=str,   default=TrainingConfig.dataset,       help='Dataset to use for training (shakespeare or tinystories)')
+    parser.add_argument('--batch_size',    type=int,   default=TrainingConfig.batch_size,    help='Batch size for training')
+    parser.add_argument('--max_iters',     type=int,   default=TrainingConfig.max_iters,     help='Maximum number of iterations for training')
+    parser.add_argument('--eval_interval', type=int,   default=TrainingConfig.eval_interval, help='Interval for evaluation')
+    parser.add_argument('--eval_iters',    type=int,   default=TrainingConfig.eval_iters,    help='Number of iterations for evaluation')
+    parser.add_argument('--learning_rate', type=float, default=TrainingConfig.learning_rate, help='Learning rate for training')
+    parser.add_argument('--warmup_steps',  type=int,   default=TrainingConfig.warmup_steps,  help='Number of warmup steps for learning rate')
+    parser.add_argument('--grad_clip',     type=float,  default=TrainingConfig.grad_clip,    help='Gradient Clip value')
+    # Model Parameters
+    parser.add_argument('--vocab_size',  type=int,   default=ModelConfig.vocab_size,  help='Vocabulary size for the model')
+    parser.add_argument('--block_size',  type=int,   default=ModelConfig.block_size,  help='Block size for the model')
+    parser.add_argument('--n_embd',      type=int,   default=ModelConfig.n_embd,      help='Embedding dimension for the model')
+    parser.add_argument('--pos_emb',     type=str,   default=ModelConfig.pos_emb,     help='Type of positional encoding (learn, sin, rope)')
+    parser.add_argument('--n_layer',     type=int,   default=ModelConfig.n_layer,     help='Number of layers in the model')
+    parser.add_argument('--dropout',     type=float, default=ModelConfig.dropout,     help='Dropout rate for the model')
+    # MLP Params
+    parser.add_argument('--up_dim',      type=int,   default=ModelConfig.up_dim,      help='Up dimension for the Expert in the model')
+    parser.add_argument('--non_linearity',type=str,   default=ModelConfig.non_linearity,help='Non-linearity for the Expert in the model')
+    # MoE Params
+    parser.add_argument('--n_exp',       type=int,   default=ModelConfig.n_exp,       help='Number of Experts in the model')
+    parser.add_argument('--n_shared',    type=int,   default=ModelConfig.n_shared,    help='Number of Shared Experts in the model')
+    parser.add_argument('--n_act',       type=int,   default=ModelConfig.n_act,       help='Number of Active Experts in the model')
+    parser.add_argument('--coeff',       type=float, default=ModelConfig.coeff,       help='Aux Loss Coefficient for the MoE if not using Aux Free')
+    parser.add_argument('--alpha',       type=float, default=ModelConfig.alpha,       help='Complementry Loss Coefficient for the MoE if using Aux Free')
+    parser.add_argument('--gamma',       type=float, default=ModelConfig.gamma,       help='Bias Update speed in Aux loss free MoE if using Aux Free')
+    # Attention Params
+    parser.add_argument('--attn',        type=str,   default=ModelConfig.attn,        help='Type of attention mechanism (mha, mqa, gqa, mla)')
+    parser.add_argument('--n_head',      type=int,   default=ModelConfig.n_head,      help='Number of attention heads in the model')
+    parser.add_argument('--n_kv_heads',  type=int,   default=ModelConfig.n_kv_heads,  help='Number of KV heads in the model (only for gqa)')
+    parser.add_argument('--q_latent_dim',  type=int, default=ModelConfig.q_latent_dim,help='Query latent dimension (only for mla)')
+    parser.add_argument('--kv_latent_dim', type=int, default=ModelConfig.kv_latent_dim,help='KV latent dimension (only for mla)')
+    parser.add_argument('--rope_head_dim', type=int, default=ModelConfig.rope_head_dim,help='RoPE head dimension (only for mla)')
+    
+    parser.add_argument('--total_batch_size_str', type=str, default=str(TrainingConfig.total_batch_size), help='Total batch size for training passed in as a string expression')
+    
+    parser.add_argument('--moe',        action='store_true', help='Whether to use Mixture of Experts in the model')
+    parser.add_argument('--aux_free',   action='store_true', help='Whether to use Aux Loss Free MoE')
+    parser.add_argument('--eval',       action='store_true', help='Wheter to perform Evalutions once a while')
+    parser.add_argument('--save_model', action='store_true', help='Whether to save the model after training')
+
+    return parser.parse_args()
+
+args = parse_args()
+for key, value in vars(args).items():
+    # need to eval the total_batch_size to get the grad_accum_steps
+    if key == 'total_batch_size_str':
+        value = eval(value)
+        setattr(TrainingConfig, 'total_batch_size', value)
+    else:
+        if isinstance(value, str) and key !='non_linearity':
+            value = value.lower().strip()
+        if hasattr(TrainingConfig, key):
+            setattr(TrainingConfig, key, value)
+        else:
+            setattr(ModelConfig, key, value)
+if ModelConfig.attn == 'mha':
+    ModelConfig.n_kv_heads = ModelConfig.n_head
+elif ModelConfig.attn == 'mqa':
+    ModelConfig.n_kv_heads = 1
+elif ModelConfig.attn == 'mla':
+    req = ModelConfig.q_latent_dim is not None and ModelConfig.kv_latent_dim is not None
+    assert req, "Either q_latent_dim or kv_latent_dim is missing"
+    if ModelConfig.pos_emb == 'rope':
+        assert ModelConfig.rope_head_dim is not None, "Need dim of Rotary heads"
+
+# _______________ DATASET _________________
+
+# Using The Tiny Shakespeare dataset for demo
+class DataLoader:
+    def __init__(self, B, T, file_path, device):
+        self.B = B ; self.T = T
+        self.file_path = file_path
+        self.device = device
+
+    def next_batch(self):
+        '''Credits to Andrej Karpathy's NanoGPT'''
+        B, T = self.B, self.T
+        tokens = np.memmap(self.file_path, dtype=np.uint16, mode='r')
+        # seed offset ensures diff data across GPUs
+        start_ix = torch.randint(len(tokens) - (B * T + 1), (1,)).item()
+        # Slice the memory-mapped array. This is where the OS reads from disk.
+        buf = tokens[start_ix : start_ix + B * T + 1]
+        # .astype(np.int64) is important as torch.LongTensor is 64-bit
+        full_tokens = torch.from_numpy(buf.astype(np.int64))
+
+        x = full_tokens[:-1].view(B, T)
+        y = full_tokens[1:].view(B, T)
+        
+        # Pin memory which allows us to move them to GPU asynchronously (non_blocking=True)
+        x, y = x.pin_memory().to(self.device, non_blocking=True), y.pin_memory().to(self.device, non_blocking=True)
+        return x,y
+
+data_dir = os.path.join('..','data', TrainingConfig.dataset)
+train_loader = DataLoader(B=TrainingConfig.batch_size, T=ModelConfig.block_size, file_path=os.path.join(data_dir, "train.bin"), device=device)
+val_loader = DataLoader(B=TrainingConfig.batch_size, T=ModelConfig.block_size, file_path=os.path.join(data_dir, "val.bin"), device=device)
+
+# ____________ UTIL FUNCTIONS _________________
+
+def get_lr(iter, TrainingConfig:Trainconfig):
+    max_lr = TrainingConfig.learning_rate
+    min_lr = max_lr*0.1
+    max_decay_steps = TrainingConfig.max_iters + 2 # avoid division by zero
+    # 1) linear warump for warmup_steps:
+    if iter < TrainingConfig.warmup_steps:
+        return max_lr * (iter+1)/TrainingConfig.warmup_steps
+    #2) if iter > lr_decay_iters, return min_lr
+    elif iter > max_decay_steps:
+        return min_lr
+    #3) in between, use cosine decay
+    else:
+        decay_ratio = (iter - TrainingConfig.warmup_steps) / (max_decay_steps - TrainingConfig.warmup_steps)
+        decay_ratio = min(decay_ratio, 1.0)  # ensure it does
+        coeff = 0.5 * (1 + math.cos(math.pi * decay_ratio))
+        return min_lr + coeff * (max_lr - min_lr)
+    
+@torch.no_grad()
+def estimate_loss(model:LLM, TrainingConfig:Trainconfig, train_loader:DataLoader, val_loader:DataLoader):
+    out = {}
+    model.eval()
+    for split, loader in [('train', train_loader), ('val', val_loader)]:
+        losses = torch.zeros(TrainingConfig.eval_iters)
+        for k in range(TrainingConfig.eval_iters):
+            X, Y = loader.next_batch() # Data is now moved to device in next_batch()
+            with ctx:
+                _, loss, _ = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+#___________GRAD_ACCUM SETUP_____________
+
+total_batch_size = TrainingConfig.total_batch_size
+B = TrainingConfig.batch_size    # microbatch size
+T = ModelConfig.block_size       # sequence length
+assert total_batch_size % (B * T *ddp_world_size) == 0, "make sure total_batch_size is divisible by B * T * ddp_world_size"
+grad_accum_steps = total_batch_size // (B * T *ddp_world_size)
+
+#___________CREATE YOUR MODEL_____________
+model = LLM(ModelConfig).to(device)
+if master_process : 
+    total, active = model.get_num_params()
+    print(f"total parameters = {total:,}, acitive parameters = {active:,}")
+
+model = DDP(model, device_ids=[ddp_local_rank], find_unused_parameters=ModelConfig.moe)
+
+if master_process : print("Using compiled model")
+model = torch.compile(model)
+
+raw_model:LLM = model.module
+
+#______________________________________________ TRAINING ______________________________________________
+
+optimizer = raw_model.configure_optimizers(weight_decay=0.1,learning_rate=TrainingConfig.learning_rate,device=device)
+x,y = train_loader.next_batch() # get the first batch of training data
+loss_stats = []
+for iter in range(TrainingConfig.max_iters+1):
+    t0 = perf_counter()
+
+    lr = get_lr(iter, TrainingConfig) 
+    for param_grp in optimizer.param_groups:
+        param_grp['lr'] = lr
+    
+    optimizer.zero_grad(set_to_none=True)
+
+    if TrainingConfig.eval and (iter % TrainingConfig.eval_interval == 0 or iter == TrainingConfig.max_iters) and iter!=0:
+        losses = estimate_loss(model, TrainingConfig, train_loader, val_loader)
+        if master_process:
+            print(f"-----val run------- train loss {losses['train']:.4f}, val loss {losses['val']:.4f} --------------")
+
+    for micro_step in range(grad_accum_steps):
+        model.require_backward_grad_sync = (micro_step == grad_accum_steps - 1)
+
+        with ctx:
+            _, loss, _ = model(x,y)
+            loss:torch.Tensor = loss/grad_accum_steps
+
+        x,y = train_loader.next_batch() # Async prefetch the next batch of data
+        loss_stats.append(loss.cpu())
+        scaler.scale(loss).backward()
+
+    if TrainingConfig.grad_clip != 0.0:
+        scaler.unscale_(optimizer)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), TrainingConfig.grad_clip)
+
+    scaler.step(optimizer)
+    scaler.update()    
+
+    if master_process:
+        torch.cuda.synchronize()
+        dt  = (perf_counter()-t0)*1000
+        print(f"step: {iter} | train loss:{loss*grad_accum_steps:.4f} | dt: {dt:.2f}ms")
+
+destroy_process_group()
+
+if TrainingConfig.save_model and master_process:
+    checkpoint = {'config': ModelConfig, 'model_state': raw_model.state_dict(), 'iter_num':iter, 'last_loss':losses, 'train_losses':loss_stats} 
+    torch.save(checkpoint, 'llm_model.pt')
+    print("checkpoint saved to llm_model.pt")
--- DDP/train.sh
+++ DDP/train.sh
@@ -1,1 +1,82 @@
-# TBA
+#!/bin/bash
+
+# This script runs the train.py Python script with specified command-line arguments.
+
+# --- Training Configuration Arguments ---
+N_GPUS=1 # For now lets assume we have single node with multiple GPUs 
+
+DATASET='tinystories'
+TOTAL_BATCH_SIZE_STR="2**13"
+BATCH_SIZE=4 #2**2
+MAX_ITERS=3000
+LEARNING_RATE=3e-4
+WARMUP_STEPS=100
+GRAD_CLIP=1.0
+EVAL=true
+EVAL_INTERVAL=250
+EVAL_ITERS=30
+SAVE_MODEL=false 
+
+# --- Model Configuration Arguments ---
+N_LAYER=12
+N_EMBD=768
+VOCAB_SIZE=50304
+BLOCK_SIZE=1024
+DROPOUT=0.1
+POS_EMB="rope" # Can be 'learn', 'sin', 'rope'
+
+UP_DIM=512
+NON_LINEARITY="gelu" # Example: 'relu', 'gelu', 'silu'
+
+ATTN="mla" # Can be 'mha', 'mqa', 'gqa', 'mla'
+N_HEAD=12
+N_KV_HEADS=4 # Only relevant if ATTN is 'gqa'
+Q_LATENT_DIM=96 # Only relevant if ATTN is 'mla'
+KV_LATENT_DIM=96 # Only relevant if ATTN is 'mla'
+ROPE_HEAD_DIM=48 # Only relevant if POS_EMB is 'rope'
+
+MOE=true
+N_EXP=16
+N_SHARED=1
+N_ACT=4
+AUX_FREE=true
+ALPHA=0.0001
+GAMMA=0.001
+CEOFF=0.01
+
+# Construct the command
+torchrun --standalone --nproc_per_node=$N_GPUS \
+    train.py \
+    --dataset $DATASET \
+    --total_batch_size_str $TOTAL_BATCH_SIZE_STR \
+    --batch_size $BATCH_SIZE \
+    --max_iters $MAX_ITERS \
+    --learning_rate $LEARNING_RATE \
+    --warmup_steps $WARMUP_STEPS \
+    --grad_clip $GRAD_CLIP \
+    --eval_interval $EVAL_INTERVAL \
+    --eval_iters $EVAL_ITERS \
+    --n_layer $N_LAYER \
+    --n_embd $N_EMBD \
+    --vocab_size $VOCAB_SIZE \
+    --block_size $BLOCK_SIZE \
+    --dropout $DROPOUT \
+    --pos_emb $POS_EMB \
+    --up_dim $UP_DIM \
+    --non_linearity $NON_LINEARITY \
+    --attn $ATTN \
+    --n_head $N_HEAD \
+    --n_kv_heads $N_KV_HEADS \
+    --q_latent_dim $Q_LATENT_DIM \
+    --kv_latent_dim $KV_LATENT_DIM \
+    --rope_head_dim $ROPE_HEAD_DIM \
+    --n_exp $N_EXP \
+    --n_shared $N_SHARED \
+    --n_act $N_ACT \
+    --alpha $ALPHA \
+    --gamma $GAMMA \
+    --coeff $CEOFF \
+    $( [ "$SAVE_MODEL" = true ] && echo "--save_model" ) \
+    $( [ "$EVAL" = true ] && echo "--eval" ) \
+    $( [ "$MOE" = true ] && echo "--moe" ) \
+    $( [ "$AUX_FREE" = true ] && echo "--aux_free" ) 
